71fe9fc69c9d836106e844cf68a84acb42b0f2db
savik2019 <59950131+savik2019@users.noreply.github.com>
2020-01-17 13:06:47 +0200

    fix user name initials logic

--- user.ex
+++ user.ex
@@ -1,14 +1,27 @@
 defmodule User do
 
-  @spec initials(name: String.t()) :: String.t()
-  def initials(name), do: select_initials(name)
-
-  @spec select_initials(name: String.t()) :: String.t()
-  def select_initials(name) when name == nil or name == "", do: "?"
-  def select_initials(name) when is_string(name) do
+  @spec initials(name) :: result when
+          name: any(),
+          result: String.t()
+  def initials(name) when name == nil or name == "", do: "?"
+  def initials(name) when is_binary(name) do
     name
     |> String.split(" ")
-    |> Enum.map_join(&String.first/1)
+    |> select_initials()
   end
+  def initials(_name), do: "Error, name should be a string"
 
+  @spec select_initials(name) :: result when
+          name: list(),
+          result: String.t()
+  defp select_initials([name]), do: String.first(name)
+  defp select_initials([first_name, last_name]), do: select_initials(first_name, last_name)
+  defp select_initials([first_name | names]), do: select_initials(first_name,  List.last(names))
+
+  @spec select_initials(first_name, last_name) :: result when
+          first_name: String.t(),
+          last_name: String.t(),
+          result: String.t()
+  defp select_initials(first_name, last_name), do: String.first(first_name) <> String.first(last_name)
+  
 end
